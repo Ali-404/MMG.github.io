801e705e1508e681e00359b2a3258c1f2fd1dcc3
Ali-404 <abdellali.aitaddi@gmail.com>
2023-11-02 17:44:31 +0100

    add first page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,40 @@
+import Link from 'next/link'
+import './main.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faContactBook, faMessage, faUser, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+
 
 
 export default function Home() {
   return (
-    <div>page</div>
+    <div className="container">
+      <section className='section'>
+        <header >
+          <img src="x.png" />
+          <ul>
+           
+            <li>
+              <Link href={'#'}>Discord</Link>
+              <FontAwesomeIcon icon={faMessage} />
+            </li>
+           
+            <li>
+              <Link href={'#'}>Login/Sign up</Link>
+              <FontAwesomeIcon icon={faUserCircle} />
+            </li>
+          
+          </ul>
+        </header>
+
+          <div className='core'>
+            <h1>Download Best MTA Mods</h1>
+            <img src='/mtasa.png'/>
+
+          </div>
+       
+
+      </section>
+    </div>
   )
 }
