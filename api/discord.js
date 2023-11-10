287428eb2da9eed2278155e72d9de8ab82baa42b
Ali-404 <abdellali.aitaddi@gmail.com>
2023-11-10 21:28:13 +0100

    #

--- api/discord.js
+++ api/discord.js
@@ -1,4 +1,4 @@
-const botToken = 'OTAxMjQxOTQ0NDQ4MDUzMjc5.GjWFOq.mQctDZ4XpqcJzRMtHljOaZ7olfbcaKqef6PpLA'; 
+const botToken = '6cEWxLoRjJcIH02paXN2nLMx1eJdtfX9'; 
 const channelId = '887736758056402984'; // Replace with your actual channel ID
 
 // api/discord.js
